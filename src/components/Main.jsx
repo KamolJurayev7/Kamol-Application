530d4c8bd7c3a0d62430d7d3d6108b9866a367c8
Kamoljuraev <kamoljurayev82@gmail.com>
2024-07-18 13:23:33 +0500

    loader add

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { Loader } from '../ui';
 
 const Main = () => {
-    const { articles } = useSelector(state => state.article)
+    const { articles, isLoading } = useSelector(state => state.article)
     return (
         <div className='container'>
+           {isLoading &&  <Loader/>}
             <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
                 {articles.map(item => ( 
                     <div className="col">
@@ -22,9 +24,10 @@ const Main = () => {
                                 </rect>
                             </svg>
                             <div className="card-body">
-                                <p className="card-text fw-bold">{item.title}</p>
+                                <p className="card-text fw-bold m-0">{item.title}</p>
                                 <p className="card-text">{item.description}</p>
-                                <div className="d-flex justify-content-between align-items-center">
+                            </div>
+                                <div className="card-footer d-flex justify-content-between align-items-center">
                                     <div className="btn-group">
                                         <button type="button" className="btn btn-sm btn-outline-success">View</button>
                                         <button type="button" className="btn btn-sm btn-outline-warning">Edit</button>
@@ -32,7 +35,6 @@ const Main = () => {
                                     </div>
                                     <small className="text-muted fw-bold text-capitalize">{item.author.username}</small>
                                 </div>
-                            </div>
                         </div>
                     </div>
                 ))}
